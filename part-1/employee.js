caa1096c28a39a9e2a1693b9e85cf4b438e943f2
Mitch Mele <mitchmele@gmail.com>
2017-10-04 10:00:51 -0500

    Add constructor functions with behavior functions for both sales team and employees.

--- part-1/employee.js
+++ part-1/employee.js
@@ -2,3 +2,25 @@
 /*   It is required to use a for loop                    */
 /*   when iterating over an employee's array of sales.   */
 /*********************************************************/
+var Employee = function(args = {}){
+  this.firstName = args.firstName;
+  this.sales = args.sales || [];
+};
+
+
+  Employee.prototype.totalSales = function(){
+  total = 0
+  for(var i = 0; i < this.sales.length; i++){
+    total+=this.sales[i]
+  }
+  return total;
+};
+
+
+Employee.prototype.averageSales = function(){
+  var average = this.totalSales();
+  return average / this.sales.length
+}
+
+
+
--- part-1/salesTeam.js
+++ part-1/salesTeam.js
@@ -2,3 +2,43 @@
 /*   It is required to use Array.prototype methods            */
 /*   when iterating over a sales team's array of employees.   */
 /**************************************************************/
+
+
+var SalesTeam = function(employees){
+  this.employees = employees;
+};
+
+
+SalesTeam.prototype.find = function(person){
+  for(var i = 0; i < this.employees.length; i++){
+    if(this.employees[i].firstName === person){
+      return this.employees[i];
+    };
+  };
+};
+
+
+SalesTeam.prototype.bonusEarners = function(){
+  var superTeamMembers = []
+  for(var i = 0; i < this.employees.length; i++ ){
+    if(this.employees[i].totalSales() > 2500){
+      superTeamMembers.push(this.employees[i])
+    };
+  };
+  return superTeamMembers;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
